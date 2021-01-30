232bd2f3bad105c91fd5ad27de4ba6a0cd89c537
agarwalashwani2020@gmail.com <agarwalashwani2020@gmail.com>
2021-01-31 01:53:11 +0530

    Added entity

---
+++ UdemyAppProject/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UdemyAppProject.Entities;
+
+namespace UdemyAppProject.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions dbContextOptions):base(dbContextOptions)
+        {
+                
+        }
+
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
